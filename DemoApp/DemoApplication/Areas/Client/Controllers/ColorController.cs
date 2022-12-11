77d787b0954049a6d4b8e435337608e4db8373ec
AslanHasanov <aslanrh@code.edu.az>
2022-12-11 22:50:00 +0400

    initial configurations, connection with sql and colorcontroller initialized

---
+++ DemoApp/DemoApplication/Areas/Client/Controllers/ColorController.cs
@@ -0,0 +1,46 @@
+﻿using DemoApplication.Areas.Client.ViewModels.Colors;
+using DemoApplication.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+namespace DemoApplication.Areas.Client.Controllers
+{
+    [Area("client")]
+    [Route("Color")]
+    public class ColorController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public ColorController(DataContext dbContext)
+        {
+            _dataContext = dbContext;
+        }
+
+
+        [HttpGet("add/{id}", Name = "client-color-add")]
+        public async Task<IActionResult> AddColorAsync([FromRoute] int id)
+        {
+            var color = await _dataContext.Colors.FirstOrDefaultAsync(c=> c.Id == id);
+
+            var colorCookieValue = HttpContext.Request.Cookies["Color"];
+
+            if (colorCookieValue is null)
+            {
+                var colorsCookieViewModel = new ColorViewModel(color!.Id,color.Name);
+
+                HttpContext.Response.Cookies.Append("Color", JsonSerializer.Serialize(colorsCookieViewModel));
+            }
+            else
+            {
+                var colorsCookieViewModel = JsonSerializer.Deserialize<ColorViewModel>(colorCookieValue);
+
+                colorsCookieViewModel = new ColorViewModel(color!.Id, color.Name);
+
+                HttpContext.Response.Cookies.Append("Color", JsonSerializer.Serialize(colorsCookieViewModel));
+            }
+
+            return RedirectToRoute("client-home-index");
+        }
+    }
+}
---
+++ DemoApp/DemoApplication/Areas/Client/ViewModels/Home/Index/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace DemoApplication.Areas.Client.ViewModels.Home.Index
+{
+    public class IndexViewModel
+    {
+        public List<BookListItemViewModel> Books { get; set; }
+
+        public List<Color> Colors { get; set; }
+
+    }
+}
